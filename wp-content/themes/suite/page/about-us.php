cab13a5c2942c9b42aa357e4e8225472fda152eb
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-08-10 11:02:25 +0700

    update about us page

--- wp-content/themes/suite/page/about-us.php
+++ wp-content/themes/suite/page/about-us.php
@@ -11,59 +11,57 @@
     <!-- name -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Commerce Name'); ?> : <?php echo get_option('commerce_name') ?></label>
+            <label><?php echo __('Commerce Name'); ?>: </label> <span><?php echo get_option('commerce_name') ?></span>
         </div>
     </div>   
     <!-- address -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Address'); ?> : <?php echo get_option('commerce_address') ?></label>
+            <label><?php echo __('Address'); ?>: </label> <span><?php echo get_option('commerce_address') ?></span>
         </div>
     </div>
     <!-- mobile -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Mobile'); ?> : <?php echo get_option('commerce_mobile') ?></label>
+            <label><?php echo __('Mobile'); ?>: </label> <span><?php echo get_option('commerce_mobile') ?></span>
         </div>
     </div>
     <!-- phone -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Phone'); ?> : <?php echo get_option('commerce_phone') ?></label>
+            <label><?php echo __('Phone'); ?>: </label> <span><?php echo get_option('commerce_phone') ?></span>
         </div>
     </div>
     <!-- fax -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Fax'); ?> : <?php echo get_option('commerce_fax') ?></label>
+            <label><?php echo __('Fax'); ?>: </label> <span><?php echo get_option('commerce_fax') ?></span>
         </div>
     </div>
     <!-- email -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Email'); ?> : <?php echo get_option('commerce_email') ?></label>
+            <label><?php echo __('Email'); ?>: </label> <span><?php echo get_option('commerce_email') ?></span>
         </div>
     </div>
     <!-- map x - map y -->
     <div class="title-cell">
-        <label><?php echo __('Maps X'); ?> : <?php echo get_option('commerce_map_x') ?></label>
+        <label><?php echo __('Maps X'); ?>: </label> <span><?php echo get_option('commerce_map_x') ?></span>
     </div>
     <div class="title-cell">
-        <label><?php echo __('Map Y'); ?> : <?php echo get_option('commerce_map_y') ?></label>
+        <label><?php echo __('Map Y'); ?>: </label> <span><?php echo get_option('commerce_map_y') ?></span>
     </div>  
 
     <!-- CUSTOM POST -->
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Chamber of Commerce'); ?>
-                : <?php get_post_meta('1', '_info_charter', TRUE) ?></label>
+            <label><?php echo __('Chamber of Commerce'); ?>: </label> <span><?php get_post_meta('1', '_info_charter', TRUE) ?></span>
         </div>
     </div>
     
     <div class="meta-row">
         <div class="title-cell">
-            <label><?php echo __('Application for membership'); ?> 
-            : <?php get_post_meta('1', '_info_apply', TRUE) ?></label>
+            <label><?php echo __('Application for membership'); ?>: </label> <span><?php get_post_meta('1', '_info_apply', TRUE) ?></span>
         </div>
     </div>
 
