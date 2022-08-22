1e82eeb5082abbe2c90b7d898393c8e514a8b165
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-08-22 10:13:33 +0700

    Update check-input.js

--- wp-content/themes/test/js/check-input.js
+++ wp-content/themes/test/js/check-input.js
@@ -36,7 +36,8 @@ jQuery(document).ready(function () {
 
   // KIEM TRA NHAP EMAIL
   jQuery(".type-email").focusout(function (e) {
-    var email = document.getElementById("txt-email");
+    //var email = document.getElementById("txt-email");
+    var email = jQuery(this).val();
     var filter =
       /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if (!filter.test(email.value)) {
