239deeb4be0782706476543855c5cb6fd4936beb
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-08-22 09:52:12 +0700

    update code

--- wp-content/themes/test/js/check-input.js
+++ wp-content/themes/test/js/check-input.js
@@ -39,7 +39,8 @@ jQuery(document).ready(function () {
 
 // KIEM TRA NHAP EMAIL
     jQuery('.type-email').focusout(function (e) {
-        var email = document.getElementById('txt-email');
+        //var email = document.getElementById('txt-email');
+        var email = jQuery(this).val();
         var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if (!filter.test(email.value)) {
             jQuery('#error-email').text('請輸入正確 E-mail 地址 ! ');
