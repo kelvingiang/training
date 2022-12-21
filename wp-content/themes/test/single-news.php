342da8372b1cb07ad71877a77254d1440377beab
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-12-21 09:57:27 +0700

    update single news

--- wp-content/themes/test/single-news.php
+++ wp-content/themes/test/single-news.php
@@ -6,7 +6,7 @@
             <h2 class="single-title"><?php the_title() ?></h2>
             <div class="sing-content "><?php the_content() ?></div>
         </div>
-        <div class="col-lg-4 col-md-12">
+        <div class="col-lg-4 col-md-12" id="archive-relate-item">
             <!-- archive -->
             <div class="single-archive-list">
                 <div class="single-archive-title">
@@ -39,21 +39,21 @@
                 <h3 class="single-relate-title"><?php echo('Bài viết liên quan') ?></h3>
                 <div class="hr3"></div>
                 <?php 
-                global $post;
-                $cate_id = array();
-                $current_category = get_the_terms($post->ID , 'news_category' );
-                foreach($current_category as $cc){
-                    $cate_id = $cc->term_id;
-                }
-                $wp_query = new WP_Query(getRelatePostNews('news', 5, $cate_id));
-                if ( $wp_query->have_posts()) { 
-                    echo  '<ul>' ; 
-                    while ( $wp_query->have_posts()) {  
-                        $wp_query->the_post()?> 
-                    <li class="single-relate-item"> 
-                        <a href = "<?php  the_permalink ();?> " > <?php the_title ( ); ?> </a> 
-                    </li> <?php }
-                    echo '</ul>' ; } else {  }
+                    global $post;
+                    $cate_id = array();
+                    $current_category = get_the_terms($post->ID , 'news_category' );
+                    foreach($current_category as $cc){
+                        $cate_id = $cc->term_id;
+                    }
+                    $wp_query = new WP_Query(getRelatePostNews('news', 5, $cate_id));
+                    if ( $wp_query->have_posts()) { 
+                        echo  '<ul>' ; 
+                        while ( $wp_query->have_posts()) {  
+                            $wp_query->the_post()?> 
+                        <li class="single-relate-item"> 
+                            <a href = "<?php  the_permalink ();?> " > <?php the_title ( ); ?> </a> 
+                        </li> <?php }
+                        echo '</ul>' ; } else {  }
                     wp_reset_postdata ();
                     wp_reset_query();
                 ?>
@@ -62,4 +62,17 @@
     </div>
     <?php get_template_part('template/template', 'single-news-latest')?>
 </div>
+<script>
+    jQuery(document).ready(function() {
+        //co dinh relate post, archive khi cuon man hinh
+        var itemTop = jQuery("#archive-relate-item").offset().top;
+        jQuery(window).scroll(function(){
+            if(jQuery(window).scrollTop() > (itemTop - 120) ){
+                jQuery("#archive-relate-item").addClass("fixed-sidebar");
+            }else{
+                jQuery("#archive-relate-item").removeClass("fixed-sidebar");
+            }
+        })
+    })
+</script>
 <?php get_footer(); ?>
